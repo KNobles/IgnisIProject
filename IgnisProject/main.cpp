2e74c0139d6358bfda8302dc753786215ad2c9c0
KNobles <nobles-k@hotmail.com>
2018-10-20 15:52:16 +0200

    Added tilemap test

--- IgnisProject/main.cpp
+++ IgnisProject/main.cpp
@@ -1,11 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include "TileMap.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works! Fine");
+    sf::RenderWindow window(sf::VideoMode(512, 256), "TileMap test");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    const int level[] =
+    {
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    };
+
+    // create the tilemap from the level definition
+    TileMap map;
+    if (!map.load("Assets/Sprites/Tiles/tileset.png", sf::Vector2u(32, 32), level, 16, 8))
+        return -1;
     while (window.isOpen())
     {
         sf::Event event;
@@ -16,7 +33,7 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(map);
         window.display();
     }
 
