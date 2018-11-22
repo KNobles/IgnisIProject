fe8a3051f6b94373dd8308d2b1f093b0590e2178
Tyrzal <la163284@student.helha.be>
2018-11-22 10:46:11 +0100

    Classe Cavalier

---
+++ IgnisProject/Model/Cavalier.h
@@ -0,0 +1,38 @@
+#ifndef CAVALIER_H
+#define CAVALIER_H
+
+#include <Character.h>
+
+using std::string;
+using std::stringstream;
+using std::cout;
+using std::endl;
+
+class Cavalier : public Character
+{
+    private:
+        static const WeaponType types[];
+
+
+    public:
+        Cavalier(string name = "xxx");
+        virtual ~Cavalier();
+        Cavalier(const Cavalier& other);
+        Cavalier& operator=(const Cavalier& other);
+
+        Cavalier* clone()const override;
+
+        void setWeapon(Weapon* weapon);
+        void addHealth();
+        void addStrength();
+        void addDefense();
+        void addSpeed();
+        void addResistance();
+        void addMagic();
+        void addLuck();
+        void addSkill();
+
+
+};
+
+#endif // CAVALIER_H
